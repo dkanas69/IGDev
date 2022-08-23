351b892eb93e7a0e0b805456dc285da190b2d43f
Anas Bin Awais <51017890+dkanas69@users.noreply.github.com>
2022-08-23 19:46:01 +1000

    80% Complete

--- Assets/ConsolePrint.cs
+++ Assets/ConsolePrint.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class ConsolePrint : MonoBehaviour
 {
+    public GameObject redObj;
+    //[SerializeField]
+    public GameObject blueObj;
+    private int i = 3;
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("Test");
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        i = i + 1;
+        Debug.Log(gameObject.name + ":" + i);
     }
 }
